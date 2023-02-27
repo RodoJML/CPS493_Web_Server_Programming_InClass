57f6700f66aaa2f21fcc9cc264a361e5fbdc2fde
RodoJML <rodojml@me.com>
2023-02-27 15:00:03 -0500

    navBar added

---
+++ client/src/components/NavBar.vue
@@ -0,0 +1,89 @@
+<script setup lang="ts"></script>
+</script>
+
+<template>
+    <div>
+        <nav class="navbar is-primary">
+            <div class="navbar-brand">
+                <a class="navbar-item" href="https://bulma.io">
+                    <img src="https://bulma.io/assets/Bulma%20Logo%20White.png"
+                        alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28">
+                </a>
+                <span class="navbar-burger" data-target="navbarMenuHeroA">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </span>
+            </div>
+            <div id="navbar" class="navbar-menu">
+                <div class="navbar-start">
+                    <a class="navbar-item">
+                        Home
+                    </a>
+
+                    <div class="navbar-item has-dropdown is-hoverable">
+                        <a class="navbar-link">
+                            Docs
+                        </a>
+
+                        <div class="navbar-dropdown">
+                            <a class="navbar-item">
+                                Overview
+                            </a>
+                            <a class="navbar-item">
+
+                                Modifiers
+                            </a>
+                            <a class="navbar-item">
+                                Columns
+                            </a>
+                            <a class="navbar-item">
+                                Layout
+                            </a>
+                            <a class="navbar-item">
+                                Form
+                            </a>
+
+                            <hr class="navbar-divider">
+                            <a class="navbar-item">
+                                Elements
+                            </a>
+                            <a class="navbar-item">
+                                Components
+                            </a>
+                        </div>
+
+                    </div>
+
+
+                </div>
+
+                <div class="navbar-end">
+                    <span class="navbar-item">
+                        <a class="button">
+                            <span class="icon">
+                                <i class="fa-brands fa-twitter"></i>
+                            </span>
+                            <span>Tweet</span>
+                        </a>
+
+                        <a class="button is-primary">
+                            <span class="icon">
+                                <i class="fa-solid fa-download"></i>
+                            </span>
+                            <span>Download</span>
+                        </a>
+                    </span>
+                </div>
+            </div>
+        </nav>
+    </div>
+</template>
+
+
+
+<style scoped></style>
+
+
+
+
--- client/src/main.ts
+++ client/src/main.ts
@@ -4,6 +4,7 @@ import router from './router'
 
 // These CSS are trying to mirror the style we use
 import './assets/main.css'
+// bulma could be imported here, but instead we did it from main.css
 
 const app = createApp(App)
 
