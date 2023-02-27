45b17e80739cdfddddd19ed76d13438486450996
RodoJML <rodojml@me.com>
2023-02-27 14:41:59 -0500

    Project cleaned up

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,7 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+// These CSS are trying to mirror the style we use
+// import './assets/main.css'
 
 const app = createApp(App)
 
