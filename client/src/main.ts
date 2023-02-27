a2f300a16dce61663903c4c52aade631bccd796d
RodoJML <rodojml@me.com>
2023-02-27 14:54:25 -0500

    bulma installed

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 
 // These CSS are trying to mirror the style we use
-// import './assets/main.css'
+import './assets/main.css'
 
 const app = createApp(App)
 
