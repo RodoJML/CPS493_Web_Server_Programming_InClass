8164169beb5e85a265f7181ab69ca807675605fe
RodoJML <rodojml@me.com>
2023-03-08 23:06:00 -0500

    updating components

--- client/src/components/Flyout.vue
+++ client/src/components/Flyout.vue
@@ -1,18 +1,28 @@
-<template>
-    <div>
-        Flyout
-    </div>
-</template>
-
 <script setup lang="ts">
 
 </script>
 
-<style scoped>
-    .flyout{
+<template>
+     <div class="flyout active">
+        <slot />
+    </div>
+</template>
 
+<style scoped>
+    .flyout {
         position: fixed;
-        top: 0;
+        top: 52px;
+        right: -18rem;
+        width: 20rem;
+        height: calc(100vh - 52px);
+        background-color: white;
+        box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.2);
+        transition: right 0.5s ease-in-out;
+    }
+    .flyout.active {
+        right: 0;
+    }
+    .flyout:hover {
         right: 0;
     }
 </style>
--- client/src/components/LoginBadge.vue
+++ client/src/components/LoginBadge.vue
@@ -1,19 +1,17 @@
 
 <script setup lang="ts">
 import { ref } from 'vue';
-import {useSession, login}from '@/model/session';
+import { useSession, login } from '@/model/session';
 const session = useSession();
 
 </script>
 
 
 <template>
-
     <div class="navbar-item" v-if="session.user">
-        Welcome, {{ session.user?.name }}
-        (<a @click=" session.user = null">Logout</a>)
+        Welcome, {{ session.user.name }}
+        (<a @click="session.user = null">logout</a>)
     </div>
-
     <div class="navbar-item" v-else>
         <a class="button is-primary" @click="login">
             <span class="icon">
@@ -21,15 +19,13 @@ const session = useSession();
             </span>
             <strong>Login</strong>
         </a>
-
-        <a class="button is-primary">
+        <a class="button is-info">
             <span class="icon">
                 <i class="fas fa-user-plus"></i>
             </span>
-            <strong>Signup</strong>
+            <strong>Sign up</strong>
         </a>
-
     </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style scoped></style>
