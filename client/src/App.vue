c2d7deb0806e0095ad96ff7110aa58fe70c4a253
RodoJML <rodojml@me.com>
2023-03-15 13:52:30 -0400

    updated all

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,19 +1,17 @@
 <script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router';
 import NavBar from './components/NavBar.vue';
-import Flyout from './components/Flyout.vue';
-import Cart from './components/Cart.vue';
+
 </script>
 
 <template>
+
   <NavBar />
 
   <div class="container">
     <RouterView />
   </div>
-
-  <Flyout>
-    <Cart />
-  </Flyout>
+  
 </template>
 
 <style scoped></style>
