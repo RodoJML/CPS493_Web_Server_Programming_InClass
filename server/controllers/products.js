28d4151c7badaa1f428a364ff20cdd96c950fa6e
RodoJML <rodojml@me.com>
2023-03-22 18:17:59 -0400

    Beginnin the API

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -1,2 +1,32 @@
-const express : RequireResolve('express');
-const router : express.Router();
+const express = require('express');
+const router = express.Router();
+
+router 
+// Write here your get, post, put, delete requests
+    .get('/', (req, res) => {
+        res.send({ 
+                items: [
+                    { 
+                        id: 1, 
+                        name: 'Product 1' 
+                    },
+                ]
+            })
+    })
+
+    .get('/:id', (req, res) => {
+    })
+
+    .post('/', (req, res) => {   
+    })
+
+    .patch('/:id', (req, res) => {
+    })
+
+    .delete('/:id', (req, res) => {
+    })
+
+    module.exports = router;
+    // module.exports is an object that is available in every JS file in the Node application
+    // module.exports is what is returned as the result of a require call
+    // module.exports is what is assigned to the exports variable
