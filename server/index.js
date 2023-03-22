838d361d66024c805bc3243d6bccf3d2fd5c70c4
RodoJML <rodojml@me.com>
2023-03-22 18:16:12 -0400

    in class updates

--- server/index.js
+++ server/index.js
@@ -1,14 +1,24 @@
 const express = require('express')
+const products = require('./controllers/products')
 const app = express()
+// These notations are equivalent to the import statements in ES6
 // Contains all exports from http 
 // Any function from HTTP is now available here
 
-const hostname = '127.0.0.1';                       // Loopbackl address
-const port = process.env.PORT || 3000;              // Commonly used as a development port
+const hostname = '127.0.0.1';
+// Loopbackl address
+const port = process.env.PORT || 3000;
+// Commonly used as a development port
 
-app.get('/', (req, res) => {
-  res.send('Hello World from Express!')
-})
+app
+  .get('/', (req, res) => {
+    res.send('Hello World from Express!')
+  })
+  .use('/products', products)
+  // .use is a method that allows us to add Middleware
+    // Middleware is a function that has access to the request and response objects
+    // Middleware is executed in the order that it is added
+    // Middleware can be added to a specific route or to all routes
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
