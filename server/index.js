56217eab05897b082ddd4967eb4f675da1e4b0ba
RodoJML <rodojml@me.com>
2023-03-23 14:07:28 -0400

    In class updates

--- server/index.js
+++ server/index.js
@@ -1,25 +1,52 @@
-const express = require('express')
-const products = require('./controllers/products')
-const app = express()
+// index.js is the entry point for the server
+// This file is the file that is run when we start the server
+// This file is the file that is run when we run the command 'npm run dev'
+
+    // 1. express
+    // Contains all exports from express
+    // Any function from express is now available here
+    const express = require('express')
+    const path = require('path')
+
+    // 2. products
+    // Contains all exports from products
+    const products = require('./controllers/products')
+
+    // 3. app
+    // The app object is a subset of the express object
+    // express() is a function that returns an app object
+    // The app object is the main object that we use to interact with express
+    const app = express()
+
 // These notations are equivalent to the import statements in ES6
-// Contains all exports from http 
-// Any function from HTTP is now available here
+// Contains all exports from http, any function from HTTP is now available here
 
+// Loopback address
 const hostname = '127.0.0.1';
-// Loopbackl address
+
+// Development Port
 const port = process.env.PORT || 3000;
-// Commonly used as a development port
 
+// Middleware
 app
-  .get('/', (req, res) => {
-    res.send('Hello World from Express!')
-  })
-  .use('/products', products)
-  // .use is a method that allows us to add Middleware
-    // Middleware is a function that has access to the request and response objects
-    // Middleware is executed in the order that it is added
-    // Middleware can be added to a specific route or to all routes
+    .use(express.json())
+    .use(express.static(path.join(__dirname, '../client/dist')))
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+
+// Actions
+app
+    .get('/api/v1/', (req, res) => {
+        res.send('Hello World! From Express')
+    })
+    .use('/api/v1/products', products)
+
+// Catch all
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'))
 })
+
+
+
+app.listen(port, () => 
+  console.log(`Server running at http://${hostname}:${port}/`)
+);
