28f4e06e0c5bb4f1183a271659949c40133ceb9b
RodoJML <rodojml@me.com>
2023-03-30 14:20:20 -0400

    update tsconfig for es2020

--- client/src/model/myFetch.ts
+++ client/src/model/myFetch.ts
@@ -1,11 +1,13 @@
+// Centralized 
 const API_URL = 'http://localhost:3000/api/v1/';
 // Depending on client or servver need to change this 
 // Create models and use these functions within
 
 export function rest(url: string){
     return fetch(url).then(res => res.json());
-}
+} // wrapper for fetch
 
 export function api(url: string){
     return rest(API_URL + url);
-}
+} // wrapper for fetch, appends URL to it
+// when access just have tpo put the name of the controller 
