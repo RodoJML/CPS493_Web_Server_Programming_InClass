5b85a34a9c31141b4225db1e2a007a63acfc1286
RodoJML <rodojml@me.com>
2023-04-08 17:15:33 -0400

    update

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -14,14 +14,13 @@ const router = createRouter({
     // which is lazy-loaded when the route is visited.
     {path: '/products', name: 'products', component: ProductsVue, beforeEnter: secureRoute},
     {path: '/login', name: 'login', component: Login},
-    {path: 'admin/products', name: 'admin-products', component: () => import('../views/admin/ProductsList.vue'), beforeEnter: secureRoute},
+    {path: '/admin/products', name: 'admin-products', component: () => import('../views/admin/ProductsList.vue'), beforeEnter: secureRoute},
     {path: '/admin/products/edit/:id?', name: 'admin-products-edit', component: () => import('../views/admin/ProductEdit.vue'), beforeEnter: secureRoute },
   ]
 })
 
 export default router
 
-//
 function secureRoute(to: RouteLocationNormalized, from: RouteLocationNormalized, next: NavigationGuardNext){
   const session = useSession();
   // This uses the global information of the active session
