c74d6ceda6346512a42dda93c900c41ffcd88cc8
RodoJML <rodojml@me.com>
2023-04-21 11:29:21 -0400

    updates to keep up to date with class

--- client/src/views/admin/productsList.vue
+++ client/src/views/admin/productsList.vue
@@ -1,35 +1,59 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import { getProducts, type Product } from '../../model/products'
+import GenModals from '../../components/GenModals.vue';
+import { confirm } from '../../model/generalModals'
 
 const products = ref<Product[]>([]);
+
 
 getProducts().then((whateverName) => {
     products.value = whateverName.data;
 });
 
+function deleteProduct(id: number) {
+    confirm('Are you sure you want to detele this', "Question",)
+    .then(() => {console.log('delete: ' + id);})
+    .catch(() => {console.log('didn\'t do it to: ' + id);})
+}
+
 </script>
 
 <template>
+    <gen-modals></gen-modals>
     <div class="admin-product-list">
-        <table class="table">
+
+        <router-link to="/admin/products/edit" class="button is-primary admin-add-product">
+            <div class="icon">
+                <i class="fa fa-plus"></i>
+            </div>
+            <span>Add Product</span>
+        </router-link>
+
+        <h1 class="title">
+            Products
+        </h1>
+
+        <table class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
             <thead>
                 <tr>
-                    <th>Title</th>
-                    <th>Price</th>
-                    <th>Category</th>
+                    <th></th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Price</th>
+                    <th scope="col">Category</th>
                     <th>Stock</th>
-                    <th>Actions</th>
+                    <th></th>
                 </tr>
             </thead>
             <tbody>
                 <tr v-for="product in products" :key="product.id">
-                    <td> <img :src="product.thumbnail" alt="" class="adminpr"></td>
+                    <td> <img :src="product.thumbnail" alt="" class="admin-product-img"></td>
                     <td>{{ product.title }}</td>
                     <td>{{ product.price }}</td>
                     <td>{{ product.category }} / {{ product.brand }}</td>
                     <td>{{ product.stock }}</td>
                     <td>
+                        <router-link></router-link>
                         <button class="button is-primary">Edit</button>
                         <button class="button is-danger">Delete</button>
                     </td>
